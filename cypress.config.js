cd5b29254df4ae11acfaeca1ab170f2bb5e85648
Marium <maryam_cis10@hotmail.com>
2025-07-29 02:38:21 -0500

    Add tests for random file selection

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
-const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("task", {
+        readDirectory(directoryPath) {
+          const fileNames = fs.readdirSync(directoryPath);
+          return fileNames.filter((file) => file.endsWith(".json"));
+        },
+        CheckIfDirectoryEmpty(directoryPath) {
+          return fs.readdirSync(directoryPath).length === 0;
+        },
+
+        readFile(filePath) {
+          const fullPath = path.resolve(filePath);
+          return fs.promises.readFile(fullPath, "utf-8");
+        },
+      });
     },
   },
-});
+};
